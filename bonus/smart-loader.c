1c468fec5169c0ae8c32b8ba7d321896a42ddd0f
xegtor <rohakkansal04@gmail.com>
2023-11-03 21:16:44 +0530

    bonus not happening.

--- bonus/smart-loader.c
+++ bonus/smart-loader.c
@@ -4,27 +4,25 @@
 
 #define PAGE_SIZE 4096
 
-typedef struct SegmentInfo{
-    Elf32_Addr fault_address;
-    size_t size;
-}SegmentInfo;
-
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd, entrypoint = -1;
-int page_faults = 0, page_allocations = 0, total_fragmentation = 0, segment = 0, fault_index = 0;
-SegmentInfo seg_info[100];
+int page_faults = 0, page_allocations = 0, total_fragmentation = 0, segment = 0;
 void *page_allocated;
+
 
 void sigsegv_handler(int signum, siginfo_t *info, void *context) {
     page_faults++;
-    uintptr_t* fault_addr = info->si_addr;
+    void* fault_addr = info->si_addr;
+    printf("Fault address: %u\n", fault_addr);
     // calculating segment in which page fault occurred
+    printf("jhskjadhlkjf:%d\n",ehdr->e_phnum);
     int i = 0;
     while(i < ehdr->e_phnum) {
         Elf32_Addr seg_start = phdr[i].p_vaddr;
         Elf32_Addr seg_end = seg_start + phdr[i].p_memsz;
-        if(fault_addr >= seg_start && fault_addr <= seg_end){
+        printf("start: %u\nend: %u\n",seg_start,seg_end);
+        if((uintptr_t)fault_addr >= seg_start && (uintptr_t)fault_addr <= seg_end){
             break;
         }
         i++;
@@ -32,16 +30,16 @@ void sigsegv_handler(int signum, siginfo_t *info, void *context) {
     // calculating address where page is needed to be inserted
     int fault_seg_index = i;
     Elf32_Addr seg_page_addr = phdr[fault_seg_index].p_vaddr;
-    while(seg_page_addr < fault_addr){
-        seg_page_addr = (void*) ((uintptr_t) seg_page_addr + PAGE_SIZE)
+    while(seg_page_addr <= fault_addr){
+        seg_page_addr = (void*) ((uintptr_t) seg_page_addr + PAGE_SIZE);
     }
     seg_page_addr = (void*) ((uintptr_t) seg_page_addr - PAGE_SIZE);
+    lseek(fd,seg_page_addr,SEEK_SET);
     // debugging start
-    // printf("Fault address: %p\n", fault_addr);
-    // printf("Faulty segment index: %d\n", fault_seg_index);
-    // printf("Faulty page: %d\n", seg_page_addr);
-    //printf("Faulty segment start: %p\n", phdr[fault_seg_index].p_vaddr);
-    //printf("Faulty segment end: %p\n", phdr[fault_seg_index].p_vaddr + phdr[fault_seg_index].p_memsz);
+    printf("Faulty segment index: %d\n", fault_seg_index);
+    printf("Faulty page: %u\n", seg_page_addr);
+    printf("Faulty segment start: %u\n", phdr[fault_seg_index].p_vaddr);
+    printf("Faulty segment end: %u\n", phdr[fault_seg_index].p_vaddr + phdr[fault_seg_index].p_memsz);
     // debugging end
     // calculating fragmentation
     uintptr_t overshoot = (uintptr_t) seg_page_addr + PAGE_SIZE;
@@ -49,14 +47,16 @@ void sigsegv_handler(int signum, siginfo_t *info, void *context) {
         overshoot -= (uintptr_t) phdr[fault_seg_index].p_vaddr + phdr[fault_seg_index].p_memsz;
     else overshoot = 0;
     total_fragmentation += overshoot;
-    // setting flags for mmap
-    int flags = MAP_PRIVATE | MAP_FIXED;
-    if (fault_seg_index != entrypoint) {
-        flags |= MAP_ANONYMOUS;
+    if (seg_page_addr == 0x0){
+        page_allocated = mmap((void*)seg_page_addr, PAGE_SIZE,PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_FIXED, fd, phdr[fault_seg_index].p_offset);
+        if (page_allocated == MAP_FAILED) {
+            perror("mmap");
+            exit(1);
+        }
+        return;
     }
     // allocating the page to resolve the page fault
-    page_allocated = mmap((void*)seg_page_addr, PAGE_SIZE,
-                          PROT_READ | PROT_WRITE | PROT_EXEC, flags, fd, 0);
+    page_allocated = mmap((void*)seg_page_addr, PAGE_SIZE,PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS, -1, 0);
     // if map fails
     if (page_allocated == MAP_FAILED) {
         perror("mmap");
